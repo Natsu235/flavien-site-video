5212a2a2423fba1ae0d157e47acdde3431f2d6ac
Natsu235 <27864198+Natsu235@users.noreply.github.com>
2021-03-17 20:36:33 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="assets/img/favicon.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/cosmo/bootstrap.min.css" integrity="sha384-5QFXyVb+lrCzdN228VS3HmzpiE7ZVwLQtkt+0d9W43LQMzz4HBnnqvVxKg6O+04d" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <title>Urban Art - Groupe 9</title>
+</head>
+
+<body>
+    <header id="header">
+        <div class="jumbotron">
+            <div class="container">
+                <div class="logo pb-5">
+                    <!--<h1><i class="fa fa-chevron-right"></i> LOGO</h1>-->
+                    <img src="assets/img/logo.png" width="320" height="171" />
+                    <hr class="my-4">
+                </div>
+                <div class="catchphrase pb-5">
+                    <h1 class="display-4">DEVOIR MONTAGE<br>GROUPE 9</h1>
+                    <br>
+                    <p class="d-sm-none d-md-block w-50" style="font-size: 2.5rem;">« Urban Art »</p>
+                    <p class="d-none d-sm-block d-md-none lead w-100" style="font-size: 2.5rem;">« Urban Art »</p>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main id="main">
+        <div style="height: 80px;"></div>
+
+        <div id="preview">
+            <div class="container pb-3">
+                <div class="d-flex justify-content-center">
+                    <!--<img class="preview" src="assets/img/fake-video.svg" alt="Preview" width="80%" height="80%" />-->
+                    <iframe src="https://www.youtube.com/watch?v=dupafpTec88" width="820" height="540"></iframe>
+                </div>
+                <br>
+                <div class="d-flex justify-content-center">
+                    <p class="lead w-75 text-center">« Nous avons décidé d'aborder un thème urbain en mélangeant la discipline du breakdance et du basket-ball dans un "décore" lui aussi urbain. Les plans sont tournés en 360° ce qui permet de se déplacer librement dans la vidéo pour découvrir à chaque visionnage un nouvel angle de vue et de nouvelles scènes. La musique quant à elle rejoint le thème principal avec une rythmique une nouvelle fois très urbaine et qui peut être raccord avec le monde du breakdance et celui du basket-ball. »</p>
+                </div>
+            </div>
+        </div>
+
+        <div style="height: 80px;"></div>
+    </main>
+
+    <footer id="footer">
+        <div class="footer-content">
+            <div class="container">
+                <p>Flavien PILORGE<br>
+                Etienne MAGUET<br>
+                Julien LE DUIGOU<br>
+                Liam LE NORMAND</p>
+            </div>
+        </div>
+    </footer>
+
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <!-- Popper.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js" integrity="sha512-BmM0/BQlqh02wuK5Gz9yrbe7VyIVwOzD1o40yi1IsTjriX/NGF37NyXHfmFzIlMmoSIBXgqDiG1VNU6kB5dBbA==" crossorigin="anonymous"></script>
+    <!-- Bootstrap -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
